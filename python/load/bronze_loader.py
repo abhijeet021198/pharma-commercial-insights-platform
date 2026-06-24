cda19afddf84f1456bf972981b72f6bb24c49684
abhijeet021198 <85667790+abhijeet021198@users.noreply.github.com>
2026-06-25 03:37:37 +0530

    Build reusable Bronze ingestion framework

--- python/load/bronze_loader.py
+++ python/load/bronze_loader.py
@@ -5,65 +5,54 @@ project_root = Path(__file__).resolve().parents[2]
 sys.path.append(str(project_root / "python"))
 
 import pandas as pd
-
 from snowflake.connector.pandas_tools import write_pandas
 
-from utils.snowflake_connection import (
-    get_snowflake_connection
+from utils.snowflake_connection import get_snowflake_connection
+
+from load.table_definitions import (
+    ACCOUNTS_DDL,
+    PRODUCTS_DDL,
+    ORDERS_DDL,
+    INVENTORY_DDL,
+    INTERACTIONS_DDL,
+    SALES_REP_DDL
 )
 
 
-def load_accounts():
+def load_csv_to_table(file_path, table_name, ddl):
 
-    print("Reading accounts.csv...")
+    print(f"\nLoading {table_name}...")
 
-    df = pd.read_csv("data/accounts.csv")
+    df = pd.read_csv(file_path)
 
     df.columns = [col.upper() for col in df.columns]
 
-    print(df.columns.tolist())
-
     conn = get_snowflake_connection()
-    
-    print("Connected to Snowflake")
 
     cursor = conn.cursor()
 
     try:
 
-        print("Creating ACCOUNTS table...")
+        cursor.execute(ddl)
 
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS ACCOUNTS (
-            ACCOUNT_ID VARCHAR,
-            ACCOUNT_NAME VARCHAR,
-            ACCOUNT_TYPE VARCHAR,
-            CITY VARCHAR,
-            STATE VARCHAR,
-            REGION VARCHAR
-        )
-        """)
-
-        print("Loading data into Snowflake...")
+        cursor.execute(
+            f"TRUNCATE TABLE {table_name}"
+            )
 
         success, nchunks, nrows, _ = write_pandas(
             conn,
             df,
-            "ACCOUNTS"
+            table_name
         )
 
-        print(f"Rows Loaded: {nrows}")
-
-        cursor.execute("""
-        SELECT COUNT(*)
-        FROM ACCOUNTS
-        """)
+        cursor.execute(
+            f"SELECT COUNT(*) FROM {table_name}"
+        )
 
         row_count = cursor.fetchone()[0]
 
+        print(f"Rows Loaded: {nrows}")
         print(f"Snowflake Row Count: {row_count}")
-
-        print("Accounts load completed successfully")
 
     finally:
 
@@ -72,4 +61,39 @@ def load_accounts():
 
 
 if __name__ == "__main__":
-    load_accounts()
+
+    load_csv_to_table(
+        "data/accounts.csv",
+        "ACCOUNTS",
+        ACCOUNTS_DDL
+    )
+
+    load_csv_to_table(
+        "data/products.csv",
+        "PRODUCTS",
+        PRODUCTS_DDL
+)
+    
+    load_csv_to_table(
+    "data/orders.csv",
+    "ORDERS",
+    ORDERS_DDL
+)
+
+load_csv_to_table(
+    "data/inventory.csv",
+    "INVENTORY",
+    INVENTORY_DDL
+)
+
+load_csv_to_table(
+    "data/interactions.csv",
+    "INTERACTIONS",
+    INTERACTIONS_DDL
+)
+
+load_csv_to_table(
+    "data/sales_rep.csv",
+    "SALES_REP",
+    SALES_REP_DDL
+)
